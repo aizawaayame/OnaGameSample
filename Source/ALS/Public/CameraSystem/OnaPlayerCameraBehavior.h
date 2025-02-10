b4d917d968cfe1ff0975725d6ad5ebbe3fff232e
aizawaayame@outlook.com <95639838+aizawaayame@users.noreply.github.com>
2025-02-10 19:40:04 +0800

    PlayerCameraBehavior Prop Init

--- Source/ALS/Public/CameraSystem/OnaPlayerCameraBehavior.h
+++ Source/ALS/Public/CameraSystem/OnaPlayerCameraBehavior.h
@@ -13,5 +13,10 @@ UCLASS()
 class ALS_API UOnaPlayerCameraBehavior : public UAnimInstance
 {
 	GENERATED_BODY()
-	
+public:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<APlayerController> PlayerController;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<APawn> Pawn;
 };
